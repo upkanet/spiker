5e7dc7fe67f296171c84b509a78ecffd26edcafc
JD <jeandamien.louise@gmail.com>
2020-09-22 21:15:57 +0200

    Plot electrode

--- app.mjs
+++ app.mjs
@@ -1,30 +1,39 @@
-import {MEARecord} from './mearecord.mjs';
+import { MEARecord } from './mearecord.mjs';
 import http from 'http';
 import fs from 'fs';
+import path from 'path';
 
 var bigfilepath = process.argv[2];
 var mear = new MEARecord(bigfilepath);
 
-async function main(){
+async function main() {
     await mear.load();
     console.log('Server available');
-    http.createServer(function(req,res){
+    http.createServer(function (req, res) {
         console.log(req.url);
-        if(req.url == '/'){
-            var d = fs.readFileSync('index.htm', 'utf-8');
-            res.write(d);
-        }
-        else if(req.url == '/favicon.ico'){
-            var d = fs.readFileSync('favicon.ico');
-            res.write(d);
-        }
-        else if(req.url.match(/^\/electrodes\/(?:([^\/]+?))\/?$/i)){
-            var eid = req.url.match(/^\/electrodes\/(?:([^\/]+?))\/?$/i)[1];
-            res.write(JSON.stringify(mear.electrodes[eid]));
+        try {
+            if (req.url == '/') {
+                var d = fs.readFileSync('index.htm', 'utf-8');
+                res.write(d);
+            }
+            else if (req.url == '/favicon.ico') {
+                var d = fs.readFileSync(req.url.substr(1));
+                res.write(d);
+            }
+            else if (req.url.match(/^\/electrodes\/(?:([^\/]+?))\/?$/i)) {
+                var eid = req.url.match(/^\/electrodes\/(?:([^\/]+?))\/?$/i)[1];
+                res.write(JSON.stringify(mear.electrodes[eid]));
+            }
+            else {
+                var d = fs.readFileSync(req.url.substr(1), 'utf-8');
+                res.write(d);
+            }
+        } catch (err) {
+            res.write('error');
         }
         res.end();
     }).listen(8080);
-    
+
 }
 
 main();
