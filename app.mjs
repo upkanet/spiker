5e99261b0dc7295f2e64071e6709374f45380d8c
JD <upkanet@gmail.com>
2021-02-15 23:15:44 +0100

    Simplified data bugfix

--- app.mjs
+++ app.mjs
@@ -1,50 +1,37 @@
-import { MEARecord } from './mearecord.mjs';
-import http from 'http';
-import fs from 'fs';
-import path from 'path';
+import { Experiment } from './record.mjs';
+import express from 'express';
+const app = express();
 
-var bigfilepath = process.argv[2];
-var mappath = process.argv[3];
-var mear = new MEARecord(bigfilepath,mappath);
+var exp = new Experiment('data/20210129', [251, 97, 247]);
+//var exp = new Experiment('data/test', [251]);
 
-async function main() {
-    await mear.load();
-    console.log('Server available http://localhost:8080/');
-    http.createServer(function (req, res) {
-        console.log(req.url);
-        try {
-            if (req.url == '/') {
-                var d = fs.readFileSync('index.htm', 'utf-8');
-                res.write(d);
-            }
-            else if (req.url == '/favicon.ico') {
-                var d = fs.readFileSync(req.url.substr(1));
-                res.write(d);
-            }
-            else if (req.url == '/emax') {
-                res.write(JSON.stringify(mear.eMax));
-            }
-            else if (req.url.match(/^\/electrodes\/(?:([^\/]+?))\/?$/i)) {
-                var eid = req.url.match(/^\/electrodes\/(?:([^\/]+?))\/?$/i)[1];
-                res.write(JSON.stringify(mear.electrodes[eid]));
-            }
-            else if (req.url.match(/^\/epos\/(?:([^\/]+?)),(?:([^\/]+?))\/?$/i)){
-                var r = req.url.match(/^\/epos\/(?:([^\/]+?)),(?:([^\/]+?))\/?$/i);
-                var x = r[1];
-                var y = r[2];
-                res.write(JSON.stringify(''+mear.electrodeAt(x,y)));
-            }
-            else {
-                var d = fs.readFileSync(req.url.substr(1), 'utf-8');
-                res.write(d);
-            }
-        } catch (err) {
-            res.write('error');
-        }
-        res.end();
-    }).listen(8080);
+app.get('/', function (req, res) {
+    res.sendFile('spiker.htm', {root : 'public'});
+});
 
-}
+app.get('/favicon.ico', function (req, res) {
+    res.sendFile('favicon.ico', {root : '.'});
+});
 
-main();
+app.get('/records', (req, res) => {
+    res.json(Object.keys(exp.records));
+});
+
+app.get('/electrodes', (req, res) => {
+    res.json(exp.electrodes);
+});
+
+app.get('/js/:js', function (req, res) {
+    res.sendFile(req.params.js, {root : 'js'});
+});
+
+app.get('/:record/:electrode', (req, res) => {
+    var r = req.params.record;
+    var e = req.params.electrode;
+    var el = exp.records[r].electrode(e);
+    res.json(el);
+});
+
+app.listen(3000)
+console.log('Server ready on port 3000');
 
